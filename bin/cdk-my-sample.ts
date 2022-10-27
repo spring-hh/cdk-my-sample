c5d79a8752d475aa936ce5aa2ddefc73113ab364
spring-hh <spring.hh.shu@gmail.com>
2022-10-28 00:13:54 +0900

    RDS/

--- bin/cdk-my-sample.ts
+++ bin/cdk-my-sample.ts
@@ -4,9 +4,11 @@ import { CdkMySampleStack } from "../lib/cdk-my-sample-stack";
 import { AlbEc2Rds } from "../lib/alb-ec2-rds";
 import { RdsSingle } from "../lib/rds-single";
 import { RdsMulti } from "../lib/rds-multi";
+import { RdsSingleReplica } from "../lib/rds-single-replica";
 
 const app = new cdk.App();
 // new CdkMySampleStack(app, "CdkMySampleStack");
 // new AlbEc2Rds(app, "AlbEc2Rds");
 // new RdsSingle(app, "RdsSingle");
 // new RdsMulti(app, "RdsMulti");
+// new RdsSingleReplica(app, "RdsSingleReplica");
